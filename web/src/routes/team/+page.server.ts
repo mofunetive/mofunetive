93178170bc09729a3f623028e229fecde7484717
Z0TEExt <zenlektomyum@gmail.com>
2023-02-19 23:12:00 +0700

    chore: remove token

--- web/src/routes/team/+page.server.ts
+++ web/src/routes/team/+page.server.ts
@@ -4,8 +4,8 @@ import { DiscordAPI } from '@mofunetive/api';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async function () {
-	const Discord = new DiscordAPI('MTA3MTI0NzI5MTI5NTQ2OTYwOA.GsnyWf.hHEFVnwP12INZT8oL3boJmNxs3sDsaCxzVX03Y');
-	const data = await Discord.GetTeamMembers();
+	const Discord = new DiscordAPI('');
+	const data = await Discord.GetTeamMembers('');
 
 	return {
 		devTeam: data.filter((data) => data.roles.includes('877250319275397130')),
