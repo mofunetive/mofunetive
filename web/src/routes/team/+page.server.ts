b7c193c9241f7e20a721c6274032bc8b0fd383b1
Faelayis <48393914+Faelayis@users.noreply.github.com>
2023-02-23 19:44:10 +0700

    chore: remove env

--- web/src/routes/team/+page.server.ts
+++ web/src/routes/team/+page.server.ts
@@ -1,11 +1,9 @@
 import { DiscordAPI } from '@mofunetive/api';
 
-import { DISCORD_TOKEN } from '$env/static/private';
-
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async function () {
-	const Discord = new DiscordAPI(DISCORD_TOKEN);
+	const Discord = new DiscordAPI();
 	const data = await Discord.GetTeamMembers();
 
 	return {
