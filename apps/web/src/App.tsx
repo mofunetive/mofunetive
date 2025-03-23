1a2035252aca50b63a47dbdee2c854d6c6c55b14
Faelayis <48393914+Faelayis@users.noreply.github.com>
2025-03-23 14:07:52 +0700

    feat: restructure web app with new components and configuration

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,13 +3,13 @@ import OutProjectSection from "./components/OurProjectSection";
 import OutTeamSection from "./components/OurTeamSection";
 
 const App = () => {
-  return (
-    <main className="container m-auto pb-96 bg-white">
-      <HomeSection />
-      <OutProjectSection />
-      <OutTeamSection />
-    </main>
-  );
+	return (
+		<main className="container m-auto pb-96 bg-white">
+			<HomeSection />
+			<OutProjectSection />
+			<OutTeamSection />
+		</main>
+	);
 };
 
 export default App;
