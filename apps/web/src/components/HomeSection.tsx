5a70c5510344338558d834e211e483002cabe3ba
Faelayis <48393914+Faelayis@users.noreply.github.com>
2025-03-28 15:37:59 +0700

    chore: clean up :/

--- apps/web/src/components/HomeSection.tsx
+++ apps/web/src/components/HomeSection.tsx
@@ -1,10 +1,7 @@
 const HomeSection = () => {
 	return (
 		<section className="min-h-screen bg-gray-300">
-			<div className="flex flex-col h-full bg-red-300 justify-center items-center gap-4">
-				<h1>Home</h1>
-				<p>Faelayis Love Loli</p>
-			</div>
+			<div className="flex flex-col h-full bg-red-300 justify-center items-center gap-4"></div>
 		</section>
 	);
 };
